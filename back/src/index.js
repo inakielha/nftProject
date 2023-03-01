86ac1c9f615e18aa6cd9f8e28bf2049c679467f2
inakielha <inakielhaiek@gmail.com>
2023-02-28 23:41:29 -0300

    change header

--- back/src/index.js
+++ back/src/index.js
@@ -14,7 +14,7 @@ app.set("port", process.env.MONGOPORT || 2500)
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*',"https://nftproject.up.railway.app"); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
